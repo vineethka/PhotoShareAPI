7b6b16cd829461e4fde3f6c78eec3a36891922e8
Vineeth Aravindan <vineetharavindan@qburst.com>
2015-02-24 15:35:11 +0530

    login webservice completed using django auth

---
+++ PhotoSharing/PhotoSharingApplication/authentication_helper.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
+def login_authenticate(request):
+    user_email = request.data['email']
+    user_password = request.data['password']
+
+    try:
+        user = User.objects.get(email=user_email)
+        if user.check_password(user_password):
+            return user
+    except User.DoesNotExist:
+        return None
+
+
+def authenticate(request):
+    user_name = request.data['user']
+    user_password = request.data['password']
+    user = authenticate(username=user_name, password=user_password)
+    if user is not None:
+        if user.is_authenticated():
+            return user
+        else:
+            return None
+    else:
+        # the authentication system was unable to verify the username and password
+        return None
--- PhotoSharing/PhotoSharingApplication/views.py
+++ PhotoSharing/PhotoSharingApplication/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import login
 from django.http import HttpResponse
-from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 
-from rest_framework import status
-
-from django.contrib.auth import authenticate
 # Create your views here.
+from PhotoSharingApplication import authentication_helper
+from PhotoSharingApplication.serializers import UserSerializer
 
 
 class JSONResponse(HttpResponse):
@@ -19,8 +18,29 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
+@api_view(['POST'])
 def login(request):
     if request.method == 'POST':
-        return HttpResponse("login")
+        user = authentication_helper.login_authenticate(request)
+        if user is not None:
+            if user.is_active:
+                # return success response
+                login(request, user)
+                serializer = UserSerializer(user)
+                return JSONResponse(get_response_data("", serializer.data))
+            else:
+                return JSONResponse(get_response_data("Invalid user name and password", ""))
+
+        else:
+            return JSONResponse(get_response_data("Invalid user name and password", ""))
+
     else:
-        return HttpResponse("not a post request")
+        return JSONResponse(get_response_data("bad request", ""))
+
+
+def get_response_data(error_message, response_data):
+    content = {'data': response_data, 'errorMessage': error_message}
+    return content
+
+
+
