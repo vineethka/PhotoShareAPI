11151c8ee47311fde88afc4c6f0fff6a90c37aeb
Vineeth Aravindan <vineetharavindan@qburst.com>
2015-02-25 14:10:13 +0530

    changed the user to photoshare

--- PhotoSharing/PhotoSharing/urls.py
+++ PhotoSharing/PhotoSharing/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/', include('PhotoSharingApplication.urls')),
+    url(r'^photoshare/', include('PhotoSharingApplication.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
