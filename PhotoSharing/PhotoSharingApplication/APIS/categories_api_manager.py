84256a1285ce2c02be555dec9d106adc839b3b58
Suslov <suslov@qburst.com>
2015-02-25 12:17:13 +0530

    conflicts resolved

--- PhotoSharing/PhotoSharingApplication/APIS/categories_api_manager.py
+++ PhotoSharing/PhotoSharingApplication/APIS/categories_api_manager.py
@@ -1,1 +1,15 @@
+from django.core.serializers import serialize
+from rest_framework.decorators import api_view
+from PhotoSharingApplication.APIS.helpers.api_helper import JSONResponse, get_response_data
+from PhotoSharingApplication.models import Categories
+
 __author__ = 'suslov'
+
+@api_view(['GET'])
+def get_all_categories(request):
+    if request.method == 'GET':
+        return JSONResponse(get_response_data("", serialize('python', Categories.objects.all())))
+    else:
+        return JSONResponse(get_response_data("bad request", ""))
+
+
--- PhotoSharing/PhotoSharingApplication/urls.py
+++ PhotoSharing/PhotoSharingApplication/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
-from PhotoSharingApplication import api
+from PhotoSharingApplication.APIS import user_manager_api, categories_api_manager
 
 urlpatterns = patterns('',
-    url(r'^login', api.login, name='login'),
-    url(r'^register', api.register, name='register'),
+    url(r'^login', user_manager_api.login, name='login'),
+    url(r'^register', user_manager_api.register, name='register'),
+    url(r'^get_all_categories', categories_api_manager.get_all_categories, name='get_all_categories'),
+
 
 )
