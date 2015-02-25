4f34ecc416f8b6331b8e8e68a75ed573e703a099
Vineeth Aravindan <vineetharavindan@qburst.com>
2015-02-25 18:39:07 +0530

    added like feature.

--- PhotoSharing/PhotoSharingApplication/APIS/picture_manager_api.py
+++ PhotoSharing/PhotoSharingApplication/APIS/picture_manager_api.py
@@ -1,4 +1,3 @@
-from datetime import date
 from rest_framework.decorators import api_view
 from PhotoSharingApplication.APIS.helpers.api_helper import JSONResponse, get_response_data
 from PhotoSharingApplication.models import PictureLikes
@@ -9,12 +8,20 @@ def like(request):
     if request.method == 'POST':
         picture_id = request.data['picture_id']
         user_id = request.data["user_id"]
+        try:
+            like = PictureLikes.objects.get(user=user_id,picture=picture_id)
+        except PictureLikes.DoesNotExist:
+            like = None
 
-        picture_like = PictureLikes()
-        picture_like.picture_id = picture_id
-        picture_like.user_id = user_id
-        picture_like.save()
-        return JSONResponse(get_response_data("", "Success"))
+        if like is None:
+            picture_like = PictureLikes()
+            picture_like.picture_id = picture_id
+            picture_like.user_id = user_id
+            picture_like.save()
+            return JSONResponse(get_response_data("", "Success"))
+        else:
+            return JSONResponse(get_response_data("User already liked this picture", ""))
+
     else:
         return JSONResponse(get_response_data("bad request", ""))
 
