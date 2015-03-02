2bc4d250b0203eaa2a7ec7afab7ef7633fae8e07
Suslov <suslov@qburst.com>
2015-03-02 14:26:57 +0530

    profile updated

---
+++ PhotoSharing/PhotoSharingApplication/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('PhotoSharingApplication', '0008_auto_20150301_2003'),
+        ('PhotoSharingApplication', '0005_auto_20150302_0508'),
+    ]
+
+    operations = [
+    ]
