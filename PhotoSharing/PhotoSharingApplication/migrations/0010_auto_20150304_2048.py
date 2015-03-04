a38a7ddffa06afde2a83126941784bdeed8b21d7
suslov <suslov@qburst.com>
2015-03-04 20:57:58 +0530

    some fixes

---
+++ PhotoSharing/PhotoSharingApplication/migrations/0010_auto_20150304_2048.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('PhotoSharingApplication', '0009_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='contest',
+            name='end_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 18, 20, 48, 21, 972225)),
+            preserve_default=True,
+        ),
+    ]
