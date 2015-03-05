e139d07d045cd67e81819fd241455e1c70fff458
TomRob3112 <robintvk@gmail.com>
2015-03-05 12:51:30 +0530

    Image details html added

---
+++ PhotoSharing/PhotoSharingApplication/migrations/0011_auto_20150304_2202.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('PhotoSharingApplication', '0010_auto_20150304_2048'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='contest',
+            name='end_at',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 18, 22, 2, 50, 783000)),
+            preserve_default=True,
+        ),
+    ]
