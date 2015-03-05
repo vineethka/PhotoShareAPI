46e613906f640f6259da01534bef12cfc242ca1e
TomRob3112 <robintvk@gmail.com>
2015-03-05 14:20:09 +0530

    image details modified

--- PhotoSharing/PhotoSharing/settings.py
+++ PhotoSharing/PhotoSharing/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'photoshare',
         'USER': 'root',
-        'PASSWORD': 'qburst',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
